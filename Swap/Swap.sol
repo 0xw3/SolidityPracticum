c6f85949b663d6924ae5f06501b561801a3d76e3
0xw3 <7857123+0xw3@users.noreply.github.com>
2024-08-03 18:31:31 +0300

    Added onlyInitialized modifier

--- Swap/Swap.sol
+++ Swap/Swap.sol
@@ -31,6 +31,11 @@ contract SimpleSwap {
     event Buy(address indexed contractSwap, address indexed buyer, IERC20Extended indexed token, uint256 tokenAmount);
     event Sell(address indexed contractSwap, address indexed seller, IERC20Extended indexed token, uint256 tokenAmount);
 
+    modifier onlyInitialized() {
+        require(isInitialized, "Swap contract has not yet been initialized");
+        _;
+    }
+
     constructor (address tokenA_, address tokenB_, uint256 ethRate_) {
         owner = msg.sender;
         tokenA = IERC20Extended(tokenA_);
@@ -68,8 +73,8 @@ contract SimpleSwap {
     * 1. Exchange rate 1 to 1
     * 2. Decimal of both tokens are equal
     */
-    function swap(IERC20 tokenLeft, uint256 amount) public {
-        require(isInitialized, "Swap contract has not yet been initialized");
+    function swap(IERC20 tokenLeft, uint256 amount) public onlyInitialized {
+
         IERC20 tokenRight = tokenLeft == tokenA ? tokenB : tokenA;
         require(isInitialized,"Contract not initialized");
         require(tokenLeft.balanceOf(msg.sender) >= amount,"You don't have enough balance");
@@ -85,8 +90,8 @@ contract SimpleSwap {
     * @dev 
     * Buy token for Ether
     */
-    function buy(IERC20Extended token) payable public {
-        require(isInitialized, "Swap contract has not yet been initialized");
+    function buy(IERC20Extended token) payable public onlyInitialized {
+
         uint256 tokenBalance = token.balanceOf(address(this));
         uint8 tokenDecimals = token.decimals();
         uint256 tokenAmountForTransfer = ( msg.value * ETHRATE * 10**tokenDecimals )/(10**18);
@@ -100,8 +105,8 @@ contract SimpleSwap {
     * @dev 
     * Sell token for Ether
     */
-    function sell(IERC20Extended token, uint256 tokenAmount) payable public {
-        require(isInitialized, "Swap contract has not yet been initialized");
+    function sell(IERC20Extended token, uint256 tokenAmount) payable public onlyInitialized {
+
         uint256 tokenBalance = token.balanceOf(msg.sender);
         uint8 tokenDecimals = token.decimals();
         uint256 etherAmount = (tokenAmount * 10**18) / (ETHRATE * 10**tokenDecimals );
